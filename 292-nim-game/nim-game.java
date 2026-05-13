572a16512cab5d15faa60c09f770468f888eb7db
R S PRATHVIR <prithvir1787@gmail.com>
2026-05-13 20:51:33 +0530

    Time: 751 ms (0.25%) | Memory: 42 MB (72.12%) - LeetSync

--- 292-nim-game/nim-game.java
+++ 292-nim-game/nim-game.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean canWinNim(int n) {
-        return n%4 !=0;
+        while(n > 0) {
+            if(n < 4) return true;
+            n -= 4;
+        }
+        return false;
     }
 }
